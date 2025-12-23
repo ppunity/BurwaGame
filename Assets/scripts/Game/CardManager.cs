3a21aece12e7a9a772607b75cdb7167b32bfc3ae
ppunity <playportunity@gmail.com>
2025-12-23 18:49:20 +0530

    dfade2

--- Assets/scripts/Game/CardManager.cs
+++ Assets/scripts/Game/CardManager.cs
@@ -329,14 +329,13 @@ public class CardManager : MonoBehaviour
         {
             StatusPanel.SetActive(true);
             statusText.text = fromTop ? "Click card pack to deal from Top" : "Click card pack to deal from Bottom";
-        }        
+        }
+        yield return new WaitForSeconds(1f);         
         StatusPanel.SetActive(false);
-        fadePanel.SetActive(true);
-        yield return new WaitForSeconds(1f); 
+        //fadePanel.SetActive(true);
+        //yield return new WaitForSeconds(1f); 
         
-        dealFromTop = fromTop;
-        
-        
+        dealFromTop = fromTop;     
         
 
         if(masterClientTag == "Dealer")
